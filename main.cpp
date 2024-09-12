fc8db3c3359434abc672880951924dc5a2fc9c97
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-12 22:42:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RUS");
+
+	int a = 0;
+
+	while (a < 20) 
+	{
+		cout << "Привет " << a << endl;
+		a++;
+	}
+
+
+
+}
